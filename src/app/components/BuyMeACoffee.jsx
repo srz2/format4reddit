e2d4f5b2bead51df1ed30a86fddc63cc567d6906
Steven Zilberberg <srz222@gmail.com>
2025-01-03 15:54:31 -0500

    Added buy me a coffee

---
+++ src/app/components/BuyMeACoffee.jsx
@@ -0,0 +1,32 @@
+"use client"
+import { useEffect } from "react";
+
+export default function BuyMeACoffee() {
+	useEffect(() => {
+		const script = document.createElement("script");
+		const div = document.getElementById("supportByBMC");
+		script.setAttribute("data-name", "BMC-Widget");
+		script.src = "https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js";
+		script.setAttribute("data-id", "kvnFNpYcl");
+		script.setAttribute("data-description", "Support me on Buy me a coffee!");
+		script.setAttribute(
+			"data-message",
+			"If this app helped you, consider buying me a coffee!",
+		);
+		script.setAttribute("data-color", "#FFDD00");
+		script.setAttribute("data-position", "Right");
+		script.setAttribute("data-x_margin", "18");
+		script.setAttribute("data-y_margin", "18");
+		script.async = true;
+		document.head.appendChild(script);
+		script.onload = function () {
+			var evt = document.createEvent("Event");
+			evt.initEvent("DOMContentLoaded", false, false);
+			window.dispatchEvent(evt);
+		};
+
+		div.appendChild(script);
+	}, []);
+
+	return <div id="supportByBMC"></div>;
+}
